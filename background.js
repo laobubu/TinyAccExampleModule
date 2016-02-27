ac747b474d996de8a13abf13695b1cd517caa432
laobubu <laobubu@gmail.com>
2016-02-27 23:21:28 +0800

    first commit

---
+++ background.js
@@ -0,0 +1,55 @@
+'use strict'
+
+const TinyAccelerator_ID = "khepfckcgmbgjgceoiliahnbidaodpjn"
+const Profile = {
+    id: "example",
+    name: "TinyAcc Example Module",
+    author: "laobubu",
+    description: "Just a blank example",
+    image: `chrome-extension://${chrome.runtime.id}/logo.png`,
+    options: `chrome-extension://${chrome.runtime.id}/options/options.html`
+}
+
+function createInstance(request) {
+    return new Promise(returnInstance => {
+        
+        var instance = {
+            view: "You selected " + request.text,
+            button: {
+                text: "Example",
+                event: {
+                    click: "this.view.textContent = 'clicked!'"
+                }
+            },
+            id: request.id
+        }
+        
+        returnInstance(instance)
+    })
+}
+
+var port = chrome.runtime.connect(TinyAccelerator_ID, { name: "module" })
+port.postMessage({
+    type: "profile",
+    profile: Profile
+})
+
+port.onMessage.addListener(function (msg) {
+    if (msg.type === "request") {
+        var request = msg.request // a InstanceRequest object
+        createInstance(request).then((instance) => {
+            if (instance !== null) {
+                port.postMessage({
+                    type: "instance",
+                    instance: instance
+                })
+            }
+        })
+    }
+    else if (msg.type === "profile") {
+        port.postMessage({
+            type: "profile",
+            profile: Profile
+        })
+    }
+})
